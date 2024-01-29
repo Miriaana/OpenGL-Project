f39240343754bc2c83373f3118269fcecd91a2ca
Miriaana <7h.miriam@gmail.com>
2024-01-29 21:41:39 +0100

    make square change color with a uniform

--- OpenGL TestProject2/res/shaders/Basic.shader
+++ OpenGL TestProject2/res/shaders/Basic.shader
@@ -15,9 +15,12 @@ void main()
 #shader fragment
 #version 330 core
 
-out vec4 FragColor;
+out vec4 color;
+
+uniform vec4 u_Color; //layout(location = 0)
 
 void main()
 {
-    FragColor = vec4(0.8f, 0.0f, 0.8f, 1.0f);
+    color = u_Color;
+    //FragColor = vec4(0.8f, 0.0f, 0.8f, 1.0f);
 };
--- OpenGL TestProject2/src/Application.cpp
+++ OpenGL TestProject2/src/Application.cpp
@@ -96,6 +96,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     /* Initialize glew (after creating current context!)*/
     if (glewInit() != GLEW_OK)
         std::cout << "Error with glew init!" << std::endl;
@@ -115,29 +117,28 @@ int main(void)
     };
 
     unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &buffer));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
     
     unsigned int ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indeces, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &ibo));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indeces, GL_STATIC_DRAW));
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-    std::cout << "VERTEX" << std::endl;
-    std::cout << source.VertexSource << std::endl;
-    std::cout << "FRAGMENT" << std::endl;
-    std::cout << source.FragmentSource << std::endl;
-
-    // "layout(location = 0) out vec4 color;\n"
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader);
+    GLCall(glUseProgram(shader));
 
-    // glBindBuffer(GL_ARRAY_BUFFER, 0);
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+    float r = 0.0f;
+    float increment = 0.05f;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -154,8 +155,16 @@ int main(void)
         glEnd();*/
         
         // glDrawArrays(GL_TRIANGLES, 0, 6);
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+
+        r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
